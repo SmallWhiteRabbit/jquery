f72f36553dce2ca9f7e30b20484a0eb6e9391549
junrong//用户名 <1377455672@qq.com//>
2018-11-30 16:35:23 +0800

    修改
    修改1

--- js/common.js
+++ js/common.js
@@ -7254,7 +7254,6 @@ var Countdown = {
         this.defaults.endTime = time;
         this.defaults.target = target;
         this.getTime()
-
     },
     getTime: function() {
         let that = this;
@@ -7296,8 +7295,6 @@ var Countdown = {
         $(this.defaults.target).empty().html(timeHtml);
         if (hour == 0 && minute == 0 && second == 0) {
             //倒计时结束后要进行的操作
-
-
         }
     },
     toDouble: function(num) {
@@ -7420,7 +7417,6 @@ let calender = {
                             $(that.defaults.showTime).empty();
                             that.showClickTime();
 
-                            // $(this).parent().siblings().find(".dayNum").removeClass("selected")
                         })
                     } else {
 
@@ -7447,7 +7443,6 @@ let calender = {
 
                         $(that.defaults.showTime).empty();
                         that.showClickTime();
-                        // $(this).parent().siblings().find(".dayNum").removeClass("selected")
                     })
                 }
 
@@ -7480,7 +7475,6 @@ let calender = {
 
                         $(that.defaults.showTime).empty();
                         that.showClickTime();
-                        // $(this).parent().siblings().find(".dayNum").removeClass("selected")
                     })
                 } else {
                     if ((i + 1) == that.defaults.clickDay && (i + 1) == that.defaults.Day && that.defaults.clickMonth == that.defaults.Month) {
@@ -7504,7 +7498,6 @@ let calender = {
 
                         $(that.defaults.showTime).empty();
                         that.showClickTime();
-                        // $(this).parent().siblings().find(".dayNum").removeClass("selected")
                     })
                 }
 
@@ -7597,4 +7590,140 @@ let calender = {
         $(".selected").trigger("click")
     }
 
+}
+
+
+/* 
+ * url 目标url 
+ * arg 需要替换的参数名称 
+ * arg_val 替换后的参数的值 
+ * return url 参数替换后的url 
+ */
+function changeURLArg(url, arg, arg_val) {
+    var pattern = arg + '=([^&]*)';
+    var replaceText = arg + '=' + arg_val;
+    if (url.match(pattern)) {
+        var tmp = '/(' + arg + '=)([^&]*)/gi';
+        tmp = url.replace(eval(tmp), replaceText);
+        return tmp;
+    } else {
+        if (url.match('[\?]')) {
+            return url + '&' + replaceText;
+        } else {
+            return url + '?' + replaceText;
+        }
+    }
+    return url + '\n' + arg + '\n' + arg_val;
+}
+/**
+ *
+ *  Base64 encode / decode
+ *
+ *  @author haitao.tu
+ *  @date   2010-04-26
+ *  @email  tuhaitao@foxmail.com
+ *
+ */
+
+function Base64() {
+    // private property
+    _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+    // public method for encoding
+    this.encode = function(input) {
+        var output = "";
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+        var i = 0;
+        input = _utf8_encode(input);
+        while (i < input.length) {
+            chr1 = input.charCodeAt(i++);
+            chr2 = input.charCodeAt(i++);
+            chr3 = input.charCodeAt(i++);
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            } else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+            output = output +
+                _keyStr.charAt(enc1) + _keyStr.charAt(enc2) +
+                _keyStr.charAt(enc3) + _keyStr.charAt(enc4);
+        }
+        return output;
+    }
+
+    // public method for decoding
+    this.decode = function(input) {
+        var output = "";
+        var chr1, chr2, chr3;
+        var enc1, enc2, enc3, enc4;
+        var i = 0;
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+        while (i < input.length) {
+            enc1 = _keyStr.indexOf(input.charAt(i++));
+            enc2 = _keyStr.indexOf(input.charAt(i++));
+            enc3 = _keyStr.indexOf(input.charAt(i++));
+            enc4 = _keyStr.indexOf(input.charAt(i++));
+            chr1 = (enc1 << 2) | (enc2 >> 4);
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            chr3 = ((enc3 & 3) << 6) | enc4;
+            output = output + String.fromCharCode(chr1);
+            if (enc3 != 64) {
+                output = output + String.fromCharCode(chr2);
+            }
+            if (enc4 != 64) {
+                output = output + String.fromCharCode(chr3);
+            }
+        }
+        output = _utf8_decode(output);
+        return output;
+    }
+
+    // private method for UTF-8 encoding
+    _utf8_encode = function(string) {
+        string = string.replace(/\r\n/g, "\n");
+        var utftext = "";
+        for (var n = 0; n < string.length; n++) {
+            var c = string.charCodeAt(n);
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            } else if ((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            } else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+
+        }
+        return utftext;
+    }
+
+    // private method for UTF-8 decoding
+    _utf8_decode = function(utftext) {
+        var string = "";
+        var i = 0;
+        var c = c1 = c2 = 0;
+        while (i < utftext.length) {
+            c = utftext.charCodeAt(i);
+            if (c < 128) {
+                string += String.fromCharCode(c);
+                i++;
+            } else if ((c > 191) && (c < 224)) {
+                c2 = utftext.charCodeAt(i + 1);
+                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                i += 2;
+            } else {
+                c2 = utftext.charCodeAt(i + 1);
+                c3 = utftext.charCodeAt(i + 2);
+                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                i += 3;
+            }
+        }
+        return string;
+    }
 }
